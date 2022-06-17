0fd91f54a3eae3a0b478d6360338b17066bc5893
ioiibryaniioi <37524191+ioiibryaniioi@users.noreply.github.com>
2022-06-18 01:36:10 +0200

    Drag and Drop

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ var file
 var nameFile
 var cuantidadArchivos = 0
 var formatAccepted = 0
-function dropHandler(ev) {
-  
-    // Impedir o comportamento padrão (impedir que o arquivo seja aberto)
-    ev.preventDefault();
-    
-    if (ev.dataTransfer.items) {
-      // Use a interface DataTransferItemList para acessar o (s) arquivo (s)
+var dropActive = false
+var archivosEnviados = false
+var block = false
 
-      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+    function dropHandler(ev) {
+    ev.preventDefault();
+    if (block){return}
+
+    if (dropActive){
+        reset()
+    }
+
+    dropActive = true
+
+    if (ev.dataTransfer.items) {
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
         cuantidadArchivos ++
         if (cuantidadArchivos > 3){
             cuantidadArchivos = 3
@@ -24,15 +31,10 @@ function dropHandler(ev) {
         if (ev.dataTransfer.items[i].kind === 'file') {
             file = ev.dataTransfer.items[i].getAsFile();
             nameFile = file.name;
-
             $('#icono-upload').hide()
-            $('#icono-upload-box').fadeIn()
             $('#info-arrastrar').html('Listado de archivos')
             $('.container-archivo').append('<div class="archivo grid content-center col-span-asdasdasdasd mx-5"><i id="icono-upload-box" class="text-[#56489c] fa-solid fa-file text-5xl mb-4 "></i><p id="statusArchivo" class="text-white text-xl">'+nameFile+'</p></div>')
-
         }
-
-
 
         formats.forEach(element => {
             let format = nameFile.split('.');
@@ -41,38 +43,61 @@ function dropHandler(ev) {
             }
             
         });
-
-      }
+        }
     } else {
       // Use a interface DataTransfer para acessar o (s) arquivo (s)
-      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-        console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-      }
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+            console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+        }
     }
   }
 
-  function enviarArchivos(){
-
-
-    
-    console.log(cuantidadArchivos);
-    console.log(formatAccepted);
+function enviarArchivos(){
+if (cuantidadArchivos > 0 && archivosEnviados == false){
     if (cuantidadArchivos == formatAccepted) {
         $('#icono-upload').hide()
         $('.container-archivo').html('')
         $('#icono-upload-ok').fadeIn()
         $('#info-arrastrar').html('Tu archivo <b>'+name+'</b> se ha subido correctamente')
-        
+        archivosEnviados = true;
         formatAccepted = false;
+        block = true
+        setTimeout(() => {
+            
+            reset()
+        }, "2000")
     } else {
-        console.log("ERROR");
-        $('#info-arrastrar').html('ERROR')
+        $('.alerta-error').fadeIn()
     }
+}
+}
 
-  }
 
-
-  function dragOverHandler(ev) {
+function dragOverHandler(ev) {
     // Impedir o comportamento padrão (impedir que o arquivo seja aberto)
     ev.preventDefault();
-  }
+}
+
+function cerrarError(){
+    $('.alerta-error').fadeOut()
+    setTimeout(() => {  
+        reset()
+    }, "500")
+}
+
+
+function reset() { 
+    $('#icono-upload-ok').hide()
+    $('#icono-upload').fadeIn()
+    $('#info-arrastrar').html('Arrastra tus archivos aquí ')
+    $('.container-archivo').html('')
+    cuantidadArchivos = 0
+    formatAccepted = 0
+    dropActive = false
+    file = ''
+    nameFile = ''
+    archivosEnviados = false
+    block = false
+
+}
+
